74e04564f764b7f1e1ad63d2930eb2fefffc8932
d-kjh <dkjh9942@gmail.com>
2025-06-30 18:26:53 +0900

    2일차

--- src/services/HttpService.js
+++ src/services/HttpService.js
@@ -2,8 +2,10 @@ import axios from 'axios';
 axios.defaults.baseURL = 'http://localhost:8080/api';
 
 class Httpservice {
-  async postMemo(item) {
-    console.log('postMemo - item: ', item);
+  async save(item) {
+    console.log('save - item: ', item);
+    const res = await axios.post('/memo', item);
+    return res.data;
   }
 
   async getMemoList(params) {
@@ -16,12 +18,17 @@ class Httpservice {
     return res.data;
   }
 
-  async getMemo(id) {
-    console.log('getMemo - id: ', id);
+  async findById(id) {
+    console.log('findById - id: ', id);
+    const res = await axios.get(`/memo/${id}`, id);
+    console.log('findById - res.data: ', res.data);
+    return res.data;
   }
 
-  async putMemo(item) {
-    console.log('putMemo - item', item);
+  async modify(item) {
+    console.log('modify - item', item);
+    const res = await axios.put('/memo', item);
+    return res.data;
   }
 
   async deleteMemo(params) {
